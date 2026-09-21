e1953a713517ab44f64bc979249222da2acc5bd3
maleksarra362-pixel <maleksarra362@gmail.com>
2026-09-21 14:20:46 +0100

    initial commit

---
+++ src/main/java/tn/esprit/tic/cce/autoloc/AutoLocApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.tic.cce.autoloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLocApplication.class, args);
+    }
+
+}
